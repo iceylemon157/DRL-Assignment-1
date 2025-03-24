4fb2be831fcb557ddd31b2600ad8af96d6ce81ab
iceylemon157 <oosheepyerd79135@gmail.com>
2025-03-24 16:13:42 +0800

    feat: add q_table

--- student_agent.py
+++ student_agent.py
@@ -4,6 +4,21 @@ import pickle
 import random
 import gym
 
+pickle_file = "q_table.pkl"
+try:
+    with open(pickle_file, "rb") as f:
+        q_table = pickle.load(f)
+except FileNotFoundError:
+    print(f"File {pickle_file} not found. Using random actions.")
+    q_table = {}
+
+def get_state_key(obs):
+    """
+    Create a unique key for the state based on its attributes.
+    This is useful for storing Q-values in a dictionary.
+    """
+    return obs
+
 def get_action(obs):
     
     # TODO: Train your own agent
@@ -12,7 +27,13 @@ def get_action(obs):
     #       To prevent crashes, implement a fallback strategy for missing keys. 
     #       Otherwise, even if your agent performs well in training, it may fail during testing.
 
+    state = get_state_key(obs)
 
-    return random.choice([0, 1, 2, 3, 4, 5]) # Choose a random action
+    if state not in q_table:
+        return random.choice([0, 1, 2, 3, 4, 5])
+
+    return np.argmax(q_table[state])
+
+    # return random.choice([0, 1, 2, 3, 4, 5]) # Choose a random action
     # You can submit this random agent to evaluate the performance of a purely random strategy.
 
